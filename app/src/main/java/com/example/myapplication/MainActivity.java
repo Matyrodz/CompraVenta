9bb5043a614d8d5104fdb8372c2fdca45968cf06
Lucas <luks671@gmail.com>
2020-07-03 02:06:15 -0300

    relacionando vistas de login

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication.ui.login.LoginRegistro;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_registrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_registrarse = (Button) findViewById(R.id.btn_registrarse);
+
+        btn_registrarse.setOnClickListener(viewRegistrarseListener);
+    }
+
+    private View.OnClickListener viewRegistrarseListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, LoginRegistro.class);
+            startActivity(intent);
+        }
+    };
+
+    public void testFragments(){
+        Fragment firsFragment = new Fragment();
+        firsFragment.setArguments(getIntent().getExtras());
+
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.layout_sesion,firsFragment)
+                .commit();
     }
 }
